53739b3b1201c06969a9da74dffa5ddde3413009
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2021-04-18 20:38:11 -0500

    feat(inspector): add source code

---
+++ src/inspector/breakline.js
@@ -0,0 +1,13 @@
+let x = 5;
+setTimeout(() => {
+  debugger;
+  const y = 20 * x; // evaluated to zero
+  console.log(y);
+}, 1000);
+x = 0;
+
+// node inspect debugger/breakline.js
+// debug> watch("x")
+// debug> cont
+// debug> watch("y")
+// debug> cont
