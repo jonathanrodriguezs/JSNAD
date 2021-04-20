2511cf7c60e3f93b9b8ec1a9823fb97c57bb8121
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2021-04-19 22:32:41 -0500

    chore: modify scope-closure-2.js

--- src/key-js-concepts/scope-closure-2.js
+++ src/key-js-concepts/scope-closure-2.js
@@ -1,9 +1,6 @@
-let counter = 0;
-
-const fibMemo = (function () {
+const fibonacci = (function () {
   const memo = [0, 1];
   function fib(n) {
-    counter += 1;
     if (typeof memo[n] !== 'number') {
       const result = fib(n - 1) + fib(n - 2);
       memo[n] = result;
@@ -13,8 +10,8 @@ const fibMemo = (function () {
   return fib;
 })();
 
-for (let i = 0; i <= 42; i += 1) {
-  console.log(`fib(${i}) = ${fibMemo(i)}`);
+if (require.main === module) {
+  for (let i = 0; i <= 42; i += 1) {
+    console.log(`fib(${i}) = ${fibonacci(i)}`);
+  }
 }
-
-console.log(`fibonacci was called ${counter} times`);
